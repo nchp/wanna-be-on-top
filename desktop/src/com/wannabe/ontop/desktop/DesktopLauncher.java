ec68a6b9169cb7a6785ce53ec3b68c0229f58aa3
Nicha Limmanee <nicha.lim@ku.th>
2017-11-10 15:34:01 +0700

    First commit.

---
+++ desktop/src/com/wannabe/ontop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.wannabe.ontop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wannabe.ontop.WannaBeOnTop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WannaBeOnTop(), config);
+	}
+}
